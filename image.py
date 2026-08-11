b38f43a5884ca5e5a6b27eb61a0c9423a34ba182
Anthony Salemo <ajssalemo@gmail.com>
2026-08-11 14:56:36 +0000

    updated image.py to use pillow and matplotlib methods due to deprecated skiimage.io imports; removed unused vars

--- image.py
+++ image.py
@@ -1,6 +1,6 @@
 import os
 import numpy as np
-from skimage.io import imread, imshow, imsave
+from PIL import Image
 import matplotlib.pyplot as plt
 from skimage.color import rgb2gray
 import helper
@@ -9,13 +9,13 @@ def process(directoryName):
     for filename in os.listdir(directoryName):
         if filename.endswith(".jpg") or filename.endswith(".png"):
             filenamestr = filename.split('.')[0]
-            extension  = filename.split('.')[1]
-            img = imread(os.path.join(directoryName, filename))
+            # matplotlib recommends using pillows Image() import. Trying to instead use matplotlib direct will get a `SyntaxError: expected png file`
+            # this code was historically using scikits methods which are now deprecated
+            # see https://matplotlib.org/stable/tutorials/images.html
+            img = Image.open(os.path.join(directoryName, filename))
             img_new = rgb2gray(img)
-            fig = plt.figure() 
-            ax1 = fig.add_subplot(221)
-            imshow(img_new)
+            plt.imshow(img_new)
             plt.title('Grayscale Format') 
-            imsave(os.getcwd() + "/output/" + filenamestr +"-" + helper.generateRandomName() + "_modified.jpeg", (img_new*255).astype(np.uint8))
+            plt.imsave(os.getcwd() + "/output/" + filenamestr +"-" + helper.generateRandomName() + "_modified.jpeg", (img_new*255).astype(np.uint8))
         else:
             continue
